52d63914daf71a44dfd0a4b4aea0537cc7b21cdb
abhinavdba92 <55071818+abhinavdba92@users.noreply.github.com>
2019-09-08 23:35:16 -0400

    new theme

--- index.php
+++ index.php
@@ -1,17 +1,50 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * The main template file.
  *
- * @package WordPress
+ * @package ThinkUpThemes
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+get_header(); ?>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+			<?php if( have_posts() ): ?>
+
+				<div id="container">
+
+				<?php while( have_posts() ): the_post(); ?>
+
+					<div class="blog-grid element<?php echo thinkup_input_stylelayout(); ?>">
+
+					<article id="post-<?php the_ID(); ?>" <?php post_class('blog-article'); ?>>
+
+						<header class="entry-header<?php thinkup_input_stylelayout_class1(); ?>">
+
+							<?php thinkup_input_blogimage(); ?>
+
+						</header>
+
+						<div class="entry-content<?php thinkup_input_stylelayout_class2(); ?><?php thinkup_input_blogcommentclass(); ?>">
+
+							<?php thinkup_input_blogtitle(); ?>
+							<?php thinkup_input_blogmeta(); ?>
+							<?php thinkup_input_blogtext(); ?>
+
+						</div><div class="clearboth"></div>
+
+					</article><!-- #post-<?php get_the_ID(); ?> -->
+
+					</div>
+
+				<?php endwhile; ?>
+
+				</div><div class="clearboth"></div>
+
+				<?php thinkup_input_pagination(); ?>
+
+			<?php else: ?>
+
+				<?php get_template_part( 'no-results', 'archive' ); ?>		
+
+			<?php endif; wp_reset_query(); ?>
+
+<?php get_footer() ?>
